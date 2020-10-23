8123b013ee2e23511422407b6fd2bfade0159761
santiago chiabrando <santiagochiabrando@hotmail.com>
2020-10-23 20:59:08 -0300

    modificado y arreglado el post. agregados comentarios varios

--- BotonWebApi/Controllers/SaveTimeController.cs
+++ BotonWebApi/Controllers/SaveTimeController.cs
@@ -14,14 +14,14 @@ namespace BotonWebApi.Controllers
 	[ApiController]
 	public class SaveTimeController : ControllerBase
 	{
-		private readonly Buttondb buttondb;
+		private readonly Buttondb buttondb; //inyeccion de dependencia
 		public SaveTimeController(Buttondb buttondb) //constructor
 		{
 			this.buttondb = buttondb;
 		}
 		// GET: api/<SaveTimeController>
 		[HttpGet]
-		public IEnumerable<Button> Get()
+		public IEnumerable<Button> Get() //nos da toda la lista botones de la db
 		{
 			var buttonList = buttondb.Buttons;
 			return buttonList;
@@ -29,7 +29,7 @@ namespace BotonWebApi.Controllers
 
 		// GET api/<SaveTimeController>/5
 		[HttpGet("{id}")]
-		public Button Get(Guid id)
+		public Button Get(Guid id) // filtra un boton de la db
 		{
 			var buttonF = buttondb.Buttons.FirstOrDefault(x => x.Id == id);
 			return buttonF;
@@ -37,21 +37,30 @@ namespace BotonWebApi.Controllers
 
 		// POST api/<SaveTimeController>
 		[HttpPost]
-		public void Post([FromBody] string value)
+		public IActionResult Post(Button button) // agrega un botton a la db y retorna el resultado de la accion
 		{
-
+			buttondb.Buttons.Add(button);
+			buttondb.SaveChanges();
+			return new CreatedResult(button.Id.ToString(), button);
 		}
 
 		// PUT api/<SaveTimeController>/5
 		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
+		public void Put(Button button)
 		{
+			var buttonA = buttondb.Buttons.FirstOrDefault(x => x.Id == button.Id);
+			buttonA.TimeClick = DateTime.Now;
+			buttondb.SaveChanges();
 		}
 
 		// DELETE api/<SaveTimeController>/5
 		[HttpDelete("{id}")]
-		public void Delete(int id)
+		public void Delete(Guid id) //elimina un boton hora de la bd
 		{
+			var buttonE = buttondb.Buttons.FirstOrDefault(y => y.Id == id);
+			buttondb.Buttons.Remove(buttonE);
+			buttondb.SaveChanges();
+
 		}
 	}
 }
--- BotonWebApi/Models/Buttondb.cs
+++ BotonWebApi/Models/Buttondb.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace BotonWebApi.Models
 {
 	public class Buttondb : DbContext
 	{
-		private readonly IConfiguration config;
+		private readonly IConfiguration config; // inyeccion de dependencia
 
-		public Buttondb(IConfiguration config) {
+		public Buttondb(IConfiguration config) { //constructor
 			this.config = config;
 		}
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer(config.GetConnectionString("Brandombase")); 
+			optionsBuilder.UseSqlServer(config.GetConnectionString("Brandombase"));
+			//optionsBuilder.UseSqlServer(config["ConnectionStrings: Brandombase"]); los dos hacen lo mismo
 		}
 		public DbSet<Button> Buttons { get; set; }
 	}
